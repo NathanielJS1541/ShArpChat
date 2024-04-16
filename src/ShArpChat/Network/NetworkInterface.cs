51f95598e37190b0bb78049e2e7e65b20ac9577c
Nathaniel Struselis <nathaniel.struselis@hotmail.co.uk>
2024-04-16 02:08:18 +0100

    [WIP] Added prototype NetworkInterface. Should this be renamed to ArpInterface?

---
+++ src/ShArpChat/Network/NetworkInterface.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace ShArpChat.Network
+{
+    public class NetworkInterface
+    {
+        public string Name { get; }
+
+        public PhysicalAddress PhysicalAddress { get; }
+
+        public string PhysicalAddressAsString => PhysicalAddress.ToString();
+
+        public IPAddress Address { get; }
+
+        public bool IsAddressValid => !Equals(Address, IPAddress.None);
+
+        public NetworkInterface(System.Net.NetworkInformation.NetworkInterface baseInterface)
+        {
+            Name = baseInterface.Name;
+            PhysicalAddress = baseInterface.GetPhysicalAddress();
+
+            var properties = baseInterface.GetIPProperties();
+            Address = properties.UnicastAddresses.FirstOrDefault(address => address.Address.AddressFamily == AddressFamily.InterNetwork)?.Address ?? IPAddress.None;
+        }
+    }
+}
