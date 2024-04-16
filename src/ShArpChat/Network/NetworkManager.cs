26ed8e07b9727f09e2e273e9602fa0ef7ae25c1c
Nathaniel Struselis <nathaniel.struselis@hotmail.co.uk>
2024-04-16 02:07:19 +0100

    [WIP] Added prototype NetworkManager

---
+++ src/ShArpChat/Network/NetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+
+namespace ShArpChat.Network
+{
+    public class NetworkManager
+    {
+        public NetworkManager()
+        {
+
+        }
+
+        public static ArpClient GetArpClientForNetwork(IPEndPoint interfaceIp)
+        {
+            return new ArpClient(interfaceIp);
+        }
+
+        public static IEnumerable<NetworkInterface> GetSupportedInterfaces()
+        {
+            var networkInterfaces = new List<NetworkInterface>();
+
+            var interfaces = System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces()
+                .Where(network => network is { SupportsMulticast: true, OperationalStatus: OperationalStatus.Up, NetworkInterfaceType: NetworkInterfaceType.Ethernet or NetworkInterfaceType.GigabitEthernet or NetworkInterfaceType.Wireless80211 });
+
+            foreach (var netInterface in interfaces)
+            {
+                networkInterfaces.Add(new NetworkInterface(netInterface));
+            }
+
+            return networkInterfaces;
+        }
+    }
+}
